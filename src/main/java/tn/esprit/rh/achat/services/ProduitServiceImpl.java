9b0318eb8da04a82b71ac5a87f55b6166afe402f
Marwa.Boudellaa <marwa.boudellaa@esprit.tn>
2022-11-14 15:30:36 +0100

    bug  produit5.1

--- src/main/java/tn/esprit/rh/achat/services/ProduitServiceImpl.java
+++ src/main/java/tn/esprit/rh/achat/services/ProduitServiceImpl.java
@@ -59,10 +59,16 @@ public class ProduitServiceImpl implements IProduitService {
 
 	@Override
 	public void assignProduitToStock(Long idProduit, Long idStock) {
+
+		Produit produit = produitRepository.findById(idProduit).orElseThrow(NullPointerException::new);
+		Stock stock = stockRepository.findById(idStock).orElseThrow(NullPointerException::new);
+		produit.setStock(stock);
+		produitRepository.save(produit);
+		/*
 		Produit produit = produitRepository.findById(idProduit).orElse(null);
 		Stock stock = stockRepository.findById(idStock).orElse(null);
 		produit.setStock(stock);
-		produitRepository.save(produit);
+		produitRepository.save(produit);*/
 
 	}
 
